14785e19bc5cfb1b856b7732f580d1e8fd2e5e62
mrdrivingduck <562655624@qq.com>
2020-05-13 14:44:52 +0800

    Refactor the test scripts for nanosecond

--- test/audit.py
+++ test/audit.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+"""Audit the cost of signature verification by VM logs."""
+
+#
+# Copyright (C) 2020, Jingtang Zhang <mrdrivingduck@gmail.com>.
+# Copyright (C) 2020, Hua Zong.
+#
+# Licensed under MIT.
+#
+
+#
+# Parse the result of vm execution into the structure.
+# The target line is like: "[   44.257384] @@@./mount@@@790614".
+#
+# file: the VM log file.
+# mapper: <ELF bin name, [time1, time2, ...]>
+#
 def parse_result(file, mapper):
     for line in file:
         line = line.strip("\n")
@@ -12,6 +28,13 @@ def parse_result(file, mapper):
                     mapper[bin_name] = [];
                 mapper[bin_name].append(exec_time)
 
+#
+# Audit the cost of signature verification for one binary.
+#
+# bin_name: the name of ELF binary.
+# sv_result_arr: the execution time array of kernel with sv.
+# no_sv_result_arr: the exection time array of kernel without sv.
+#
 def audit_sv_cost(bin_name, sv_result_arr, no_sv_result_arr):
     if len(sv_result_arr) != len(no_sv_result_arr):
         raise Exception("Execution time inconsistent.")
