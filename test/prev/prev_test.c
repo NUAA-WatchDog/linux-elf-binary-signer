b15c03ea1371a375b4b89f4152ea46e45d1a5190
mrdrivingduck <562655624@qq.com>
2020-08-29 11:57:51 +0800

    Preformance test C program

---
+++ test/prev/prev_test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/fcntl.h>
+
+char EXEC_COUNT[] = "100";
+char *TARGETS[] = {
+    "cp", "df", "echo", "false", "grep", "kill", "less", "ls", "mkdir",
+    "mount", "mv", "rm", "rmdir", "tar", "touch", "true", "umount", "uname"
+};
+
+int main()
+{
+    struct timeval tv;
+    long long interval;
+
+    for (int i = 0; i < sizeof(TARGETS) / sizeof(char *); i++) {
+
+        gettimeofday(&tv,NULL);
+        interval = tv.tv_sec * 1000000 + tv.tv_usec;
+
+        int pid = fork();
+        if (pid == 0) {
+            // int fd = open("/dev/null", O_WRONLY);
+            // dup2(fd, STDOUT_FILENO);
+            // dup2(fd, STDERR_FILENO);
+            // close(fd);
+
+            char *argv[] = { "./prev_test.sh", TARGETS[i], EXEC_COUNT, NULL };
+            execvp("./prev_test.sh", argv);
+        }
+        waitpid(pid, NULL, 0);
+
+        gettimeofday(&tv,NULL);
+        interval = tv.tv_sec * 1000000 + tv.tv_usec - interval;
+
+        printf("%s done in %lld μs.\n", TARGETS[i], interval);
+    }
+
+    return 0;
+}
--- test/prev/prev_test.sh
+++ test/prev/prev_test.sh
@@ -3,27 +3,20 @@
 mkdir prev_test || (echo "cleaning"; rm -r prev_test; mkdir prev_test)
 cd prev_test
 
-targets=("cp" "df" "echo" "false" "grep" "kill" "less" "ls" "mkdir" \
-    "mount" "mv" "rm" "rmdir" "tar" "touch" "true" "umount" "uname" )
-exec_count=1000
+target=$1 # From cmd parameter
+# targets=("cp" "df" "echo" "false" "grep" "kill" "less" "ls" "mkdir" \
+#     "mount" "mv" "rm" "rmdir" "tar" "touch" "true" "umount" "uname" )
+exec_count=$2
 
-for target in "${targets[@]}"
+echo
+cp /bin/${target} ./
+../../../elf-sign.signed sha256 \
+    ../../../certs/kernel_key.pem ../../../certs/kernel_key.pem ./${target} > /dev/null
+
+echo "*** Executing signed version of ${target} for ${exec_count} times."
+for ((i = 0; i < ${exec_count}; i++))
 do
-    echo
-    cp /bin/${target} ./
-    ../../../elf-sign.signed sha256 \
-        ../../../certs/kernel_key.pem ../../../certs/kernel_key.pem ./${target} > /dev/null
-
-    echo "*** Executing signed version of ${target} for ${exec_count} times."
-    for ((i = 0; i < ${exec_count}; i++))
-    do
-        ./${target} > /dev/null 2>&1
-    done
-
-    # echo "*** Executing original version of ${target} for ${exec_count} times."
-    # for ((i = 0; i < ${exec_count}; i++))
-    # do
-    #     ./${target}.old > /dev/null 2>&1
-    # done
-    echo "*** ${target} done."
+    ./${target} > /dev/null 2>&1
 done
+
+echo "*** ${target} done."
